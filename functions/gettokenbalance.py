3d85c0796538431ee2b204f45c9c1d1d7e9ca95a
Shreyas T <97907864+Shreyas-ITB@users.noreply.github.com>
2024-08-18 10:43:14 +0530

    Add NATI

--- functions/gettokenbalance.py
+++ functions/gettokenbalance.py
@@ -8,3 +8,21 @@ def get_token_balance(token_contract, decimals):
     response = requests.get(url).json()
     balance = Decimal(response['result']) / (10 ** decimals)
     return balance
+
+def getnatisupply():
+    url = "https://api.etherscan.io/api"
+    params = {
+        "module": "stats",
+        "action": "tokensupply",
+        "contractaddress": "0x4f14e88b5037f0ca24348fa707e4a7ee5318d9d5",
+        "apikey": ETHERSCANAPI
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if data["status"] == "1":
+            return Decimal(int(data["result"]) / (10 ** 18))
+        else:
+            raise ValueError(f"Error: {data['message']}")
+    else:
+        raise Exception(f"HTTP Error: {response.status_code}")
