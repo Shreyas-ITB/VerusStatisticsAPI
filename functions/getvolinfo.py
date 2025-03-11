842484e3faacffd383de906ad7982df9a60e3224
Shreyas T <97907864+Shreyas-ITB@users.noreply.github.com>
2025-03-11 11:35:00 +0530

    Update getvolinfo.py

--- functions/getvolinfo.py
+++ functions/getvolinfo.py
@@ -63,3 +63,34 @@ def calculatevolumeinfo():
         data[basket] = volume_info
 
     return data
+
+def getcurrencyvolumeinfo_new(currency, fromblock, endblock, interval, volumecurrency, target_currency_id):
+    requestData = {
+        "method": "post",
+        "url": RPCURL,
+        "headers": {"Content-Type": "application/json"},
+        "data": {
+            "method": "getcurrencystate",
+            "params": [currency, f"{fromblock}, {endblock}, {interval}", volumecurrency],
+            "id": 1
+        }
+    }
+    response = send_request(**requestData)
+    if not isinstance(response, dict) or 'result' not in response or not isinstance(response['result'], list) or not response['result']:
+        return None
+
+    result_list = response['result']
+    if not isinstance(result_list[0], dict) or 'currencystate' not in result_list[0] or not isinstance(result_list[0]['currencystate'], dict):
+        return None
+
+    currencystate = result_list[0]['currencystate']
+    if 'reservecurrencies' not in currencystate or not isinstance(currencystate['reservecurrencies'], list):
+        return None
+
+    reserve_currencies = currencystate['reservecurrencies']
+
+    for currency_info in reserve_currencies:
+        if isinstance(currency_info, dict) and currency_info.get('currencyid') == target_currency_id:
+            return currency_info.get('reserves')
+
+    return None
